bdce849871de44f9762098539beb4bb268b64ea6
Dib <79500311+tdib@users.noreply.github.com>
2025-02-02 19:04:30 +1100

    Update API URL

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -34,7 +34,7 @@
 
   onMount(() => {
     // socket = io("http://localhost:3000")
-    socket = io("https://api.dilemma-delight.tdib.xyz")
+    socket = io("https://api-dilemma-delight.tdib.xyz")
 
     socket.on("userListUpdate", (newUserList: User[]) => {
       console.log("received from server:", newUserList);
